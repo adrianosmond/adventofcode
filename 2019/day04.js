b9286585b03a3ed9139fff292f775362fb117c1e
Adrian Osmond <a.c.osmond@gmail.com>
2019-12-04 06:44:33 +0100

    2019 day 4

---
+++ 2019/day04.js
@@ -0,0 +1,29 @@
+const [from, to] = require('./input04');
+
+const matches = [0, 0];
+
+const testPassword = password => {
+  let number = password;
+  let lastDigit = 9;
+
+  const freq = new Array(10).fill(0);
+  while (number > 1) {
+    const digit = number % 10;
+    freq[digit] += 1;
+    if (digit > lastDigit) {
+      return [false, false];
+    }
+    number = (number - digit) / 10;
+    lastDigit = digit;
+  }
+  return [Math.max(...freq) > 1, freq.indexOf(2) >= 0];
+};
+
+for (let i = from; i <= to; i += 1) {
+  const [ascendingWithConsecutive, ascendingWithStrictDouble] = testPassword(i);
+  matches[0] += ascendingWithConsecutive ? 1 : 0;
+  matches[1] += ascendingWithStrictDouble ? 1 : 0;
+}
+
+console.log('part1:', matches[0]);
+console.log('part2:', matches[1]);
