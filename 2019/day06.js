6f6b4717e5931066e9927e5dbe2baa6e53ef6927
Adrian Osmond <a.c.osmond@gmail.com>
2019-12-06 08:41:49 +0100

    2019 day 6

---
+++ 2019/day06.js
@@ -0,0 +1,38 @@
+const input = require('./input06'); // multi line string
+
+const orbits = input.split('\n').map(x => x.split(')'));
+const chains = new Array(orbits.length);
+
+const comIndex = orbits.findIndex(o => o[0] === 'COM');
+chains[comIndex] = [orbits[comIndex]];
+
+const getParentIndex = parent => orbits.findIndex(o => o[1] === parent);
+
+const getChain = start => {
+  const chain = [];
+  let parentIndex = getParentIndex(start);
+  const startIndex = parentIndex;
+  while (!chains[parentIndex]) {
+    const parent = orbits[parentIndex];
+    chain.push(parent);
+    parentIndex = getParentIndex(parent[0]);
+  }
+  chain.push(...chains[parentIndex]);
+  chains[startIndex] = chain;
+  return chain;
+};
+
+const getUniqueSegments = (chainA, chainB) => [
+  ...chainA.filter(c => !chainB.includes(c)),
+  ...chainB.filter(c => !chainA.includes(c)),
+];
+
+const numOrbits = orbits
+  .map(o => getChain(o[1]).length)
+  .reduce((a, b) => a + b, 0);
+
+const pathLength =
+  getUniqueSegments(getChain('YOU'), getChain('SAN')).length - 2;
+
+console.log('part1:', numOrbits);
+console.log('part2:', pathLength);
