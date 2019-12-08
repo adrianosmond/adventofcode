272d6019f9eb84d4fd4a5b4cdbb477d64ef1347b
Adrian Osmond <a.c.osmond@gmail.com>
2019-12-08 08:15:50 +0100

    2019 day 8 Clean up

--- 2019/day08.js
+++ 2019/day08.js
@@ -1,15 +1,17 @@
-const input = require('./input08');
+const input = require('./input08'); // String
 
+const OUTPUT_CHARS = [' ', '#']; // Black and white pixels
 const IMG_WIDTH = 25;
 const IMG_HEIGHT = 6;
+const PX_PER_LAYER = IMG_WIDTH * IMG_HEIGHT;
 const pixels = input.split('').map(p => parseInt(p, 10));
-const numPixels = pixels.length;
-const pxPerLayer = IMG_WIDTH * IMG_HEIGHT;
-const numLayers = numPixels / pxPerLayer;
+const numLayers = pixels.length / PX_PER_LAYER;
 
 const layers = new Array(numLayers)
   .fill()
-  .map((_, i) => pixels.slice(i * pxPerLayer, i * pxPerLayer + pxPerLayer));
+  .map((_, i) =>
+    pixels.slice(i * PX_PER_LAYER, i * PX_PER_LAYER + PX_PER_LAYER),
+  );
 
 const day8part1 = () =>
   layers
@@ -18,7 +20,7 @@ const day8part1 = () =>
       score: l.filter(x => x === 1).length * l.filter(x => x === 2).length,
     }))
     .reduce((best, current) => (current.zeros >= best.zeros ? best : current), {
-      zeros: pxPerLayer,
+      zeros: PX_PER_LAYER,
       score: -1,
     }).score;
 
@@ -26,9 +28,9 @@ const day8part2 = () =>
   layers
     .reduce(
       (img, layer) => img.map((px, i) => (px < 2 ? px : layer[i])),
-      new Array(pxPerLayer).fill(2),
+      new Array(PX_PER_LAYER).fill(2),
     )
-    .map(px => [' ', '#'][px])
+    .map(px => OUTPUT_CHARS[px])
     .reduce(
       (img, px, idx) => img + px + ((idx + 1) % IMG_WIDTH === 0 ? '\n' : ''),
     );
