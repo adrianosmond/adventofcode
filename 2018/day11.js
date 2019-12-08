b096b4f4fd3cdd48e3ada8f69c6e7b97b4bf12e5
Adrian Osmond <a.c.osmond@gmail.com>
2019-12-08 11:14:51 +0100

    2018 day 12 (plus day 11 clean up)

---
+++ 2018/day11.js
@@ -0,0 +1,72 @@
+const serial = require('./input11');
+
+const getRackId = x => x + 10;
+
+const getPower = (x, y) => {
+  let power = getRackId(x);
+  power *= y;
+  power += serial;
+  power *= getRackId(x, y);
+  power = Math.floor(power / 100) % 10;
+  power -= 5;
+  return power;
+};
+
+const grid = [];
+for (let i = 0; i < 300; i++) {
+  grid[i] = [];
+  for (let j = 0; j < 300; j++) {
+    grid[i][j] = getPower(j + 1, i + 1);
+  }
+}
+
+function getScore(x, y, size) {
+  let score = 0;
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      score += grid[y + i][x + j];
+    }
+  }
+
+  return score;
+}
+
+const getBestScore = (size = 3) => {
+  let bestScore = Number.MIN_SAFE_INTEGER;
+  let bestX = -1;
+  let bestY = -1;
+
+  for (let i = 0; i < 300 - (size - 1); i++) {
+    for (let j = 0; j < 300 - (size - 1); j++) {
+      const score = getScore(j, i, size);
+
+      if (score > bestScore) {
+        bestScore = score;
+        bestX = j + 1;
+        bestY = i + 1;
+      }
+    }
+  }
+  return [`${bestX},${bestY}`, bestScore];
+};
+
+const getBestSize = () => {
+  let bestSize = -1;
+  let bestCoords;
+  let bestScore = Number.MIN_SAFE_INTEGER;
+  for (let i = 1; i <= 300; i++) {
+    const [coords, score] = getBestScore(i);
+    if (score > bestScore) {
+      bestCoords = coords;
+      bestScore = score;
+      bestSize = i;
+    }
+    if (score < 0) {
+      return `${bestCoords},${bestSize}`;
+    }
+  }
+  throw new Error('Something went wrong');
+};
+
+console.log('part1:', getBestScore()[0]);
+console.log('part2:', getBestSize());
---
+++ 2018/day12.js
@@ -0,0 +1,87 @@
+const input = require('./input12.js');
+
+const initialState = input.initialState.split('').map(x => x === '#');
+const stateRules = input.rules.split('\n').map(rule => rule.split(' => '));
+const buffer = 150;
+const minRepetitions = 10;
+const bufferedPlants = [
+  ...Array(buffer).fill(false),
+  ...initialState,
+  ...Array(buffer).fill(false),
+];
+
+const matchesPattern = (state, middle, pattern) =>
+  state[middle - 2] === pattern[0] &&
+  state[middle - 1] === pattern[1] &&
+  state[middle] === pattern[2] &&
+  state[middle + 1] === pattern[3] &&
+  state[middle + 2] === pattern[4];
+
+const rules = stateRules.map(r => {
+  const [pat, res] = r;
+  const pattern = pat.split('').map(x => x === '#');
+  return (state, middle) =>
+    matchesPattern(state, middle, pattern) ? res === '#' : null;
+});
+
+const transform = (state, middle) => {
+  for (let i = 0; i < rules.length; i++) {
+    const val = rules[i](state, middle);
+    if (val !== null) {
+      return val;
+    }
+  }
+  throw new Error("Couldn't transform");
+};
+
+const countPlants = plants =>
+  plants.reduce((acc, curr, idx) => acc + (curr ? idx - buffer : 0), 0);
+
+const day12part1 = () => {
+  const plants = [[...bufferedPlants], [...bufferedPlants]];
+
+  let gen = 0;
+  for (gen = 0; gen < 20; gen++) {
+    const currentGenIdx = gen % 2;
+    const nextGenIdx = (currentGenIdx + 1) % 2;
+    const start = plants[currentGenIdx].indexOf(true) - 3;
+    const end = plants[currentGenIdx].lastIndexOf(true) + 3;
+    for (let i = start; i <= end; i++) {
+      plants[nextGenIdx][i] = transform(plants[currentGenIdx], i);
+    }
+  }
+  return countPlants(plants[gen % 2]);
+};
+
+const day12part2 = () => {
+  const plants = [[...bufferedPlants], [...bufferedPlants]];
+
+  let gen = 0;
+  const diffs = Array(minRepetitions).fill(Number.MAX_SAFE_INTEGER);
+  let prevCount = -1;
+
+  while (gen < buffer) {
+    const currentGenIdx = gen % 2;
+    const nextGenIdx = (currentGenIdx + 1) % 2;
+    const start = plants[currentGenIdx].indexOf(true) - 3;
+    const end = plants[currentGenIdx].lastIndexOf(true) + 3;
+    for (let i = start; i <= end; i++) {
+      plants[nextGenIdx][i] = transform(plants[currentGenIdx], i);
+    }
+    const count = countPlants(plants[gen % 2]);
+    if (prevCount > 0) {
+      diffs[gen % diffs.length] = count - prevCount;
+      if (diffs.filter(d => d !== diffs[0]).length === 0) {
+        return (50000000000 - gen) * diffs[0] + count;
+      }
+    }
+    prevCount = count;
+    gen++;
+  }
+  throw new Error(
+    'No pattern found. Try increasing buffer and/or minRepetitions',
+  );
+};
+
+console.log('part1:', day12part1());
+console.log('part2:', day12part2());
