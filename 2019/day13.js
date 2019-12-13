3f732f433035004c863caed7b783d05c9498c441
Adrian Osmond <a.c.osmond@gmail.com>
2019-12-13 08:10:33 +0100

    2019 day 13

---
+++ 2019/day13.js
@@ -0,0 +1,41 @@
+const input = require('./input13');
+const { intComputer, getNOutputs } = require('./intComputer');
+
+const day12part1 = () => {
+  const computer = intComputer(input, []);
+  const grid = [];
+  for (const [x, y, output] of getNOutputs(3, computer)) {
+    grid[`${x},${y}`] = output;
+  }
+  return Object.values(grid).filter(v => v === 2).length;
+};
+
+const day12part2 = () => {
+  const program = [...input];
+  program[0] = 2;
+  const inputs = [];
+  const computer = intComputer(program, inputs);
+
+  let score = 0;
+  let paddleX;
+
+  for (const [x, , output] of getNOutputs(3, computer)) {
+    if (x === -1) {
+      score = output;
+    } else if (output === 3) {
+      paddleX = x;
+    } else if (output === 4) {
+      if (paddleX) {
+        if (x < paddleX) {
+          inputs.push(-1);
+        } else if (x > paddleX) {
+          inputs.push(1);
+        }
+      }
+    }
+  }
+  return score;
+};
+
+console.log('part1:', day12part1());
+console.log('part2:', day12part2());
