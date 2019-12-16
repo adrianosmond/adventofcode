ae2320cc29aae13b261a2c747441696d664f0a34
adrianosmond <adrian.osmond@ev-box.com>
2019-12-16 11:15:26 +0100

    2019 day 16 part 2

--- 2019/day16.js
+++ 2019/day16.js
@@ -1,5 +1,5 @@
-// const input = require('./input16');
-const input = '12345678';
+const input = require('./input16');
+
 const signal = input.split('').map(x => parseInt(x, 10));
 const phases = [signal, new Array(signal.length).fill(0)];
 const base = [0, 1, 0, -1];
@@ -8,7 +8,6 @@ const getMultiplier = (phase, digit) => {
   const pos = Math.floor((digit + 1) / (phase + 1)) % base.length;
   return base[pos];
 };
-console.log(input);
 
 for (let phase = 0; phase < 100; phase++) {
   const p = phase % 2;
@@ -20,7 +19,26 @@ for (let phase = 0; phase < 100; phase++) {
     }
     phases[pNext][i] = Math.abs(total) % 10;
   }
-  console.log(phases[pNext].join(''));
 }
 
 console.log('part1:', phases[0].slice(0, 8).join(''));
+
+let input10000 = '';
+for (let i = 0; i < 10000; i++) {
+  input10000 += input;
+}
+
+const start = parseInt(input.substr(0, 7), 10);
+input10000 = input10000
+  .substr(start)
+  .split('')
+  .map(x => parseInt(x, 10));
+
+for (let phase = 0; phase < 100; phase++) {
+  for (let i = input10000.length - 2; i >= 0; i--) {
+    input10000[i] += input10000[i + 1];
+    input10000[i] %= 10;
+  }
+}
+
+console.log('part2:', input10000.slice(0, 8).join(''));
