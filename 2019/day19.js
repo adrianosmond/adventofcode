774903d601c770c70e4f5f085d6dec4e134c38f5
adrianosmond <adrian.osmond@ev-box.com>
2019-12-19 18:42:05 +0100

    2019 day 19 part 2

--- 2019/day19.js
+++ 2019/day19.js
@@ -11,3 +11,22 @@ for (let y = 0; y < SIZE; y++) {
 }
 
 console.log('part1:', total);
+
+const SIZE_2 = 99;
+let x = 0;
+let y = SIZE_2;
+
+while (true) {
+  const bottomLeft = intComputer(input, [x, y]).next().value;
+  if (bottomLeft) {
+    const topRight = intComputer(input, [x + SIZE_2, y - SIZE_2]).next().value;
+    if (topRight) {
+      console.log('part2:', 10000 * x + (y - SIZE_2));
+      break;
+    } else {
+      y++;
+    }
+  } else {
+    x++;
+  }
+}
