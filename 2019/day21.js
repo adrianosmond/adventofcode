2680cd8bb4b5ab5917b22642c0266d5f2945948f
Adrian Osmond <a.c.osmond@gmail.com>
2019-12-21 08:52:56 +0100

    2019 day 21

---
+++ 2019/day21.js
@@ -0,0 +1,33 @@
+const input = require('./input21');
+const { intComputer } = require('./intComputer');
+
+const toAsciiArray = str => str.split('').map(char => char.charCodeAt(0));
+
+const springBot = instructions => {
+  const ascii = toAsciiArray([...instructions, ''].join('\n'));
+  const computer = intComputer(input, ascii);
+  let output;
+  for (output of computer);
+  return output;
+};
+
+const day21part1 = () =>
+  springBot(['NOT C J', 'AND D J', 'NOT A T', 'OR T J', 'WALK']);
+
+const day21part2 = () =>
+  springBot([
+    'NOT A J',
+    'NOT B T',
+    'OR T J',
+    'NOT C T',
+    'OR T J',
+    'AND D J',
+    'NOT E T',
+    'NOT T T',
+    'OR H T',
+    'AND T J',
+    'RUN',
+  ]);
+
+console.log('part1:', day21part1());
+console.log('part2:', day21part2());
