f52d2f0a17ed18b423d77ec09cb94f3e0840419d
Adrian Osmond <a.c.osmond@gmail.com>
2019-12-23 18:59:11 +0100

    2017 day 3 - 4

---
+++ 2017/day04.js
@@ -0,0 +1,27 @@
+const input = require('./input04');
+
+const passphrases = input.split('\n').map(r => r.split(' ').sort());
+const alphabetised = passphrases.map(r =>
+  r
+    .map(w =>
+      w
+        .split('')
+        .sort()
+        .join(''),
+    )
+    .sort(),
+);
+
+const removeDuplicates = words => {
+  for (let i = 0; i < words.length - 1; i++) {
+    if (words[i] === words[i + 1]) {
+      return 0;
+    }
+  }
+  return 1;
+};
+
+const sum = (tot, curr) => tot + curr;
+
+console.log('part1:', passphrases.map(removeDuplicates).reduce(sum));
+console.log('part2:', alphabetised.map(removeDuplicates).reduce(sum));
