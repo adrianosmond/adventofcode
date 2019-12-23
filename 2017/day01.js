8a9350a1fdf1a32049dbe5887ffdef65d607cc19
Adrian Osmond <a.c.osmond@gmail.com>
2019-12-23 18:44:39 +0100

    2017 days 1 & 2

---
+++ 2017/day01.js
@@ -0,0 +1,23 @@
+const input = require('./input01');
+
+const captcha = input.split('').map(x => parseInt(x, 10));
+
+console.log(
+  'part1;',
+  captcha.reduce(
+    (tot, curr, idx) =>
+      tot + (curr === captcha[(idx + 1) % captcha.length] ? curr : 0),
+    0,
+  ),
+);
+console.log(
+  'part2;',
+  captcha.reduce(
+    (tot, curr, idx) =>
+      tot +
+      (curr === captcha[(idx + captcha.length / 2) % captcha.length]
+        ? curr
+        : 0),
+    0,
+  ),
+);
---
+++ 2017/day02.js
@@ -0,0 +1,29 @@
+const input = require('./input02');
+
+const digits = input.split('\n').map(r =>
+  r
+    .split(' ')
+    .map(x => parseInt(x, 10))
+    .sort((a, b) => a - b),
+);
+
+const result = digits
+  .map(r => r[r.length - 1] - r[0])
+  .reduce((tot, curr) => tot + curr, 0);
+console.log('part1:', result);
+
+const result2 = digits
+  .map(r => {
+    for (let i = 0; i < r.length - 1; i++) {
+      const x = r[i];
+      for (let j = i + 1; j < r.length; j++) {
+        const y = r[j];
+        if (y % x === 0) {
+          return y / x;
+        }
+      }
+    }
+    throw new Error('Not found');
+  })
+  .reduce((tot, curr) => tot + curr, 0);
+console.log('part2:', result2);
