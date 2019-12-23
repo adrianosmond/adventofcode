2b75742442d70938389e3788ccbe86ea43235142
Adrian Osmond <a.c.osmond@gmail.com>
2019-12-23 13:35:19 +0100

    2018 day 19

---
+++ 2018/day19.js
@@ -0,0 +1,114 @@
+const input = require('./input19');
+
+const [p, ...inst] = input.split('\n');
+const ptrReg = parseInt(p.split(' ')[1], 10);
+const instructions = inst
+  .map(i => i.split(' '))
+  .map(([i, ...values]) => [i, ...values.map(v => parseInt(v, 10))]);
+
+const findFactors = n => {
+  let factor = 2;
+  const factors = [];
+  while (n >= factor) {
+    if (n % factor === 0) {
+      factors.push(factor);
+    }
+    factor++;
+  }
+  return factors;
+};
+
+/* eslint-disable no-param-reassign */
+const opCodes = {
+  addr: (reg, A, B, C) => {
+    reg[C] = reg[A] + reg[B];
+  },
+  addi: (reg, A, B, C) => {
+    reg[C] = reg[A] + B;
+  },
+  mulr: (reg, A, B, C) => {
+    reg[C] = reg[A] * reg[B];
+  },
+  muli: (reg, A, B, C) => {
+    reg[C] = reg[A] * B;
+  },
+  banr: (reg, A, B, C) => {
+    reg[C] = reg[A] & reg[B];
+  },
+  bani: (reg, A, B, C) => {
+    reg[C] = reg[A] & B;
+  },
+  borr: (reg, A, B, C) => {
+    reg[C] = reg[A] | reg[B];
+  },
+  bori: (reg, A, B, C) => {
+    reg[C] = reg[A] | B;
+  },
+  setr: (reg, A, B, C) => {
+    reg[C] = reg[A];
+  },
+  seti: (reg, A, B, C) => {
+    reg[C] = A;
+  },
+  gtir: (reg, A, B, C) => {
+    reg[C] = A > reg[B] ? 1 : 0;
+  },
+  gtri: (reg, A, B, C) => {
+    reg[C] = reg[A] > B ? 1 : 0;
+  },
+  gtrr: (reg, A, B, C) => {
+    reg[C] = reg[A] > reg[B] ? 1 : 0;
+  },
+  eqir: (reg, A, B, C) => {
+    reg[C] = A === reg[B] ? 1 : 0;
+  },
+  eqri: (reg, A, B, C) => {
+    reg[C] = reg[A] === B ? 1 : 0;
+  },
+  eqrr: (reg, A, B, C) => {
+    reg[C] = reg[A] === reg[B] ? 1 : 0;
+  },
+};
+/* eslint-enable no-param-reassign */
+
+const day19part1 = () => {
+  const registers = [0, 0, 0, 0, 0, 0];
+  let ptr = 0;
+  let instruction;
+  let A;
+  let B;
+  let C;
+  while (ptr >= 0 && ptr < instructions.length) {
+    registers[ptrReg] = ptr;
+    [instruction, A, B, C] = instructions[ptr];
+    opCodes[instruction](registers, A, B, C);
+    ptr = registers[ptrReg];
+    ptr++;
+  }
+  return registers[0];
+};
+
+/* After analysis it looks like the program is finding the factors of
+   the large number and summing them up. Let's run the program until
+   the large number is in one of the registers and then do the
+   factoring and summing ourselves */
+const day19part2 = () => {
+  const registers = [1, 0, 0, 0, 0, 0];
+  let ptr = 0;
+  let instruction;
+  let A;
+  let B;
+  let C;
+  for (let i = 0; i < 20; i++) {
+    registers[ptrReg] = ptr;
+    [instruction, A, B, C] = instructions[ptr];
+    opCodes[instruction](registers, A, B, C);
+    ptr = registers[ptrReg];
+    ptr++;
+  }
+  const factors = findFactors(Math.max(...registers));
+  return factors.reduce((a, b) => a + b, 0);
+};
+
+console.log('part1:', day19part1());
+console.log('part2:', day19part2());
