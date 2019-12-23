27cc980fb75181c9d2e7f4ed1864759c1269003f
Adrian Osmond <a.c.osmond@gmail.com>
2019-12-23 22:22:51 +0100

    2017 day 8 - 9

---
+++ 2017/day08.js
@@ -0,0 +1,39 @@
+const input = require('./input08')
+  .split('\n')
+  .map(inst => inst.split(' '));
+
+const reg = {};
+const ops = {
+  '<': (a, b) => a < b,
+  '<=': (a, b) => a <= b,
+  '>': (a, b) => a > b,
+  '>=': (a, b) => a >= b,
+  '==': (a, b) => a === b,
+  '!=': (a, b) => a !== b,
+};
+
+let max = 0;
+
+input.forEach(instruction => {
+  const [target, action, amt, , condTarget, condition, condAmt] = instruction;
+  const amount = parseInt(amt, 10);
+  const condAmount = parseInt(condAmt, 10);
+  if (!reg[target]) {
+    reg[target] = 0;
+  }
+  if (!reg[condTarget]) {
+    reg[condTarget] = 0;
+  }
+  if (ops[condition](reg[condTarget], condAmount)) {
+    if (action === 'inc') {
+      reg[target] += amount;
+    }
+    if (action === 'dec') {
+      reg[target] -= amount;
+    }
+    max = Math.max(max, reg[target]);
+  }
+});
+
+console.log('part1:', Math.max(...Object.values(reg)));
+console.log('part2:', max);
---
+++ 2017/day09.js
@@ -0,0 +1,44 @@
+const input = require('./input09');
+
+let sanitised = input.replace(/!./g, '');
+let inputLength = sanitised.length;
+let prevInputLength = 0;
+let removedInputLength = 2;
+
+while (inputLength !== prevInputLength) {
+  prevInputLength = inputLength;
+  sanitised = sanitised.replace(/<[^>]*>/, '');
+  inputLength = sanitised.length;
+  removedInputLength += prevInputLength - inputLength;
+  removedInputLength -= 2;
+}
+
+const buildStructure = start => {
+  const structure = { children: [] };
+  for (let i = start + 1; i < sanitised.length; i++) {
+    if (sanitised[start] !== '{') throw new Error('Huh?');
+    structure.start = start;
+    if (sanitised[i] === '}') {
+      structure.end = i;
+      return structure;
+    }
+    if (sanitised[i] === '{') {
+      const child = buildStructure(i);
+      i = child.end;
+      structure.children.push(child);
+    }
+  }
+  throw new Error('Not found');
+};
+
+const score = (pts, structure) => {
+  return (
+    pts +
+    structure.children
+      .map(c => score(pts + 1, c))
+      .reduce((tot, cur) => tot + cur, 0)
+  );
+};
+
+console.log('part1:', score(1, buildStructure(0)));
+console.log('part2:', removedInputLength);
