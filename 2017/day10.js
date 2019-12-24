f2e9d98076389af087ec2a7ada9f112a1ac98cf4
Adrian Osmond <a.c.osmond@gmail.com>
2019-12-24 15:21:51 +0100

    2017 day 10

---
+++ 2017/day10.js
@@ -0,0 +1,71 @@
+const input = require('./input10');
+
+const day10part1 = inputStr => {
+  const list = new Array(256).fill().map((_, i) => i);
+  const inputArr = inputStr.split(',').map(d => parseInt(d, 10));
+
+  let pos = 0;
+  let skip = 0;
+
+  for (let j = 0; j < inputArr.length; j++) {
+    const clone = list.slice(0);
+    const len = inputArr[j];
+    let end = pos + len - 1;
+    if (end >= list.length) end -= list.length;
+
+    for (let i = 0; i < len; i++) {
+      let inverse = end - i;
+      if (inverse < 0) inverse += list.length;
+      list[(pos + i) % list.length] = clone[inverse];
+    }
+    pos += len + skip;
+    pos %= list.length;
+    skip++;
+  }
+  return list[0] * list[1];
+};
+
+const knotHash = inputStr => {
+  const list = new Array(256).fill().map((_, i) => i);
+  const inputArr = inputStr
+    .split('')
+    .map(c => c.charCodeAt(0))
+    .concat([17, 31, 73, 47, 23]);
+
+  let pos = 0;
+  let skip = 0;
+
+  for (let round = 0; round < 64; round++) {
+    for (let j = 0; j < inputArr.length; j++) {
+      const clone = list.slice(0);
+      const len = inputArr[j];
+      let end = pos + len - 1;
+      if (end >= list.length) end -= list.length;
+
+      for (let i = 0; i < len; i++) {
+        let inverse = end - i;
+        if (inverse < 0) inverse += list.length;
+        list[(pos + i) % list.length] = clone[inverse];
+      }
+      pos += len + skip;
+      pos %= list.length;
+      skip++;
+    }
+  }
+
+  let hash = '';
+
+  for (let outer = 0; outer < list.length; outer += 16) {
+    let total = 0;
+    for (let inner = 0; inner < 16; inner++) {
+      total ^= list[outer + inner];
+    }
+    hash += total < 16 ? `0${total.toString(16)}` : total.toString(16);
+  }
+  return hash;
+};
+
+console.log('part1:', day10part1(input));
+console.log('part2:', knotHash(input));
+
+module.exports = knotHash;
