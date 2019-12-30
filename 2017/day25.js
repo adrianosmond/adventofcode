11cba9f1c507f23ba328321da08ceb6ea8cdb8b8
Adrian Osmond <a.c.osmond@gmail.com>
2019-12-30 14:24:24 +0100

    2017 day 25

---
+++ 2017/day25.js
@@ -0,0 +1,32 @@
+const { startState, states, checksumAfter } = require('./input25');
+/**
+ * states should be an object which takes the form: {
+    A: [ // In state A
+      { // If value === 0
+        write: 1,
+        move: 1,
+        state: 'B',
+      },
+      { // If value === 1
+        write: 0,
+        move: -1,
+        state: 'B',
+      },
+    ],
+    ...
+  }
+ */
+
+let ptr = 100000;
+const tape = new Array(ptr * 2).fill(0);
+let currentState = startState;
+
+for (let i = 0; i < checksumAfter; i++) {
+  const { write, move, state } = states[currentState][tape[ptr]];
+  tape[ptr] = write;
+  ptr += move;
+  currentState = state;
+}
+
+const checksum = tape.reduce((a, b) => a + b);
+console.log('part1:', checksum);
