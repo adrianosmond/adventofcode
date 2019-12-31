365a5570b028893157cfd3a9f7b85f668cad72d2
Adrian Osmond <a.c.osmond@gmail.com>
2019-12-31 12:03:48 +0100

    2016 day 5

---
+++ 2016/day05.js
@@ -0,0 +1,31 @@
+const md5 = require('./md5');
+const input = require('./input05');
+
+let password = '';
+let password2 = '________';
+
+let i = 0;
+let charsSolved = 0;
+
+while (charsSolved < 8) {
+  const hash = md5.hashBinary(`${input}${i}`);
+
+  if (hash.startsWith('00000')) {
+    if (password.length < 8) {
+      password += hash.substr(5, 1);
+      if (password.length === 8) {
+        console.log('part1:', password);
+      }
+    }
+    const pos = parseInt(hash.substr(5, 1), 10);
+    if (pos < password2.length && password2[pos] === '_') {
+      password2 = password2.split('');
+      password2[pos] = hash.substr(6, 1);
+      password2 = password2.join('');
+      charsSolved++;
+    }
+  }
+  i++;
+}
+
+console.log('part2:', password2);
