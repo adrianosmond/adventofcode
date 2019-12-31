e4ff12e82c9e2b966973d0a6f99d57264e0c7731
Adrian Osmond <a.c.osmond@gmail.com>
2019-12-31 09:25:44 +0100

    2016 day 3

---
+++ 2016/day03.js
@@ -0,0 +1,26 @@
+const input = require('./input03');
+
+const digits = input
+  .split('\n')
+  .map(s => s.trim())
+  .map(s => s.split(/\s+/).map(d => parseInt(d, 10)));
+
+const isTriangle = (a, b, c) => a + b > c && a + c > b && b + c > a;
+
+const validTriangles = digits.filter(row => isTriangle(...row));
+
+console.log('part1:', validTriangles.length);
+
+let count = 0;
+for (let row = 0; row < digits.length; row += 3) {
+  for (let col = 0; col < 3; col++) {
+    const a = digits[row][col];
+    const b = digits[row + 1][col];
+    const c = digits[row + 2][col];
+    if (isTriangle(a, b, c)) {
+      count++;
+    }
+  }
+}
+
+console.log('part2:', count);
