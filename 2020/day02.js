7e892a82a0039fe2210771940df69a886781c1c6
Adrian Osmond <a.c.osmond@gmail.com>
2020-12-02 09:00:04 +0100

    2020 day 2

---
+++ 2020/day02.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const path = require('path');
+
+const input = fs
+  .readFileSync(path.resolve(__dirname, 'input02.txt'), 'utf8')
+  .split('\n')
+  .map((str) => {
+    const [, num1, num2, letter, password] = str.match(
+      /(\d+)-(\d+) ([a-z]): ([a-z]+)/,
+    );
+    return [parseInt(num1, 10), parseInt(num2, 10), letter, password];
+  });
+
+const isValidPassword = (min, max, letter, password) => {
+  const letterCount =
+    password.length - password.replace(new RegExp(letter, 'g'), '').length;
+
+  return letterCount >= min && letterCount <= max;
+};
+
+const isValidPassword2 = (pos1, pos2, letter, password) => {
+  return (password[pos1 - 1] === letter) ^ (password[pos2 - 1] === letter);
+};
+
+const part1 = () => {
+  return input.reduce((total, pass) => {
+    return total + (isValidPassword(...pass) ? 1 : 0);
+  }, 0);
+};
+
+const part2 = () => {
+  return input.reduce((total, pass) => {
+    return total + (isValidPassword2(...pass) ? 1 : 0);
+  }, 0);
+};
+
+console.log('part1', part1());
+console.log('part2', part2());
