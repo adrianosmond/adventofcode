1487401a030ff398dbcf07f1eaa8af3c3e25fe9e
Adrian Osmond <a.c.osmond@gmail.com>
2020-12-03 06:30:09 +0100

    2020 day 3

---
+++ 2020/day03.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+
+const input = fs
+  .readFileSync(path.resolve(__dirname, 'input03.txt'), 'utf8')
+  .split('\n');
+
+const treesEncountered = (downMovement, rightMovement) => {
+  const width = input[0].length;
+  let row = 0;
+  let col = 0;
+  let numTrees = 0;
+  while (row < input.length) {
+    if (input[row][col] === '#') numTrees++;
+    col += rightMovement;
+    col %= width;
+    row += downMovement;
+  }
+  return numTrees;
+};
+
+const part1 = () => treesEncountered(1, 3);
+
+const part2 = () =>
+  treesEncountered(1, 1) *
+  treesEncountered(1, 3) *
+  treesEncountered(1, 5) *
+  treesEncountered(1, 7) *
+  treesEncountered(2, 1);
+
+console.log('part1', part1());
+console.log('part2', part2());
