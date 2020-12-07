832628286920ad8a6ec166698277f23cb2b3c10b
Adrian Osmond <a.c.osmond@gmail.com>
2020-12-07 23:27:19 +0100

    2020 day 7 cleanup

--- 2020/day07.js
+++ 2020/day07.js
@@ -5,64 +5,55 @@ const { mergeObjects } = require('../utils/reducers');
 const input = fs.readFileSync(path.resolve(__dirname, 'input07.txt'), 'utf8');
 
 const rules = input.split('\n');
+
+const contentsToObject = (bags) => {
+  if (bags === 'no other bags.') {
+    return {};
+  }
+
+  const [, num, bagColor] = bags
+    .replace(/\sbag[s]?\.?/, '')
+    .match(/(\d) ([a-z\s]+)/);
+
+  return {
+    [bagColor]: parseInt(num, 10),
+  };
+};
+
 const ruleEntries = rules.map((rule) => {
   const [color, contents] = rule.split(' bags contain ');
   return [
     color,
-    {
-      hasPath: false,
-      contents:
-        contents === 'no other bags.'
-          ? {}
-          : contents
-              .split(', ')
-              .map((innerBags) => {
-                const [, num, bagColor] = innerBags
-                  .replace(/\sbag[s]?\.?/, '')
-                  .match(/(\d) ([a-z\s]+)/);
-
-                return {
-                  [bagColor]: parseInt(num, 10),
-                };
-              })
-              .reduce(mergeObjects),
-    },
+    contents.split(', ').map(contentsToObject).reduce(mergeObjects),
   ];
 });
+
 const ruleTree = Object.fromEntries(ruleEntries);
-const bagColor = 'shiny gold';
 
 const findContaining = (target) =>
   ruleEntries
-    .filter(([, contents]) => contents.contents[target])
+    .filter(([, contents]) => contents[target])
     .map(([key]) => key)
     .flat();
 
 const getNumberOfBags = (target, count) =>
   count +
   count *
-    Object.entries(ruleTree[target].contents).reduce((total, [key, number]) => {
+    Object.entries(ruleTree[target]).reduce((total, [key, number]) => {
       return total + getNumberOfBags(key, number);
     }, 0);
 
 const part1 = () => {
-  const toProcess = new Set(findContaining(bagColor));
-  const processed = new Set([bagColor]);
+  const containingBagColor = new Set(findContaining('shiny gold'));
 
-  for (const key of toProcess.keys()) {
-    processed.add(key);
-
-    findContaining(key)
-      .filter((newKey) => !processed.has(newKey) && !toProcess.has(newKey))
-      .forEach((newKey) => toProcess.add(newKey));
-
-    toProcess.delete(key);
+  for (const key of containingBagColor.keys()) {
+    findContaining(key).forEach((newKey) => containingBagColor.add(newKey));
   }
 
-  return processed.size - 1;
+  return containingBagColor.size;
 };
 
-const part2 = () => getNumberOfBags(bagColor, 1) - 1;
+const part2 = () => getNumberOfBags('shiny gold', 1) - 1;
 
 console.log('part1', part1());
 console.log('part2', part2());
