2256832c523ace68c62f0040f7f462175c9565c1
Adrian Osmond <a.c.osmond@gmail.com>
2020-12-10 05:55:23 +0100

    2020 day 9

---
+++ 2020/day09.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const path = require('path');
+
+const input = fs.readFileSync(path.resolve(__dirname, 'input09.txt'), 'utf8');
+const numbers = input.split('\n').map((n) => parseInt(n, 10));
+const PREAMBLE = 25;
+
+const part1 = () => {
+  for (let i = PREAMBLE; i < numbers.length; i++) {
+    const toValidate = numbers[i];
+    const previousNumbers = numbers.slice(i - PREAMBLE, i);
+
+    for (let j = 0; j < previousNumbers.length; j++) {
+      if (
+        !previousNumbers.some((number) =>
+          previousNumbers.includes(toValidate - number),
+        )
+      ) {
+        return toValidate;
+      }
+    }
+  }
+};
+
+const part2Target = part1();
+
+const part2 = () => {
+  for (let i = 0; i < numbers.length; i++) {
+    let total = 0;
+    let min = Number.MAX_SAFE_INTEGER;
+    let max = Number.MIN_SAFE_INTEGER;
+    for (let j = i; j < numbers.length; j++) {
+      total += numbers[j];
+      min = Math.min(min, numbers[j]);
+      max = Math.max(max, numbers[j]);
+      if (total === part2Target) {
+        return min + max;
+      }
+      if (total > part2Target) {
+        j = numbers.length;
+      }
+    }
+  }
+};
+
+console.log('part1', part2Target);
+console.log('part2', part2());
