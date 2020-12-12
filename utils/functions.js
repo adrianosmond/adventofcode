cc9e53b8d5f9dfd74b41c7beb81e57809083d303
adrianosmond <adrian.osmond@ev-box.com>
2020-12-12 07:24:46 +0100

    2020 day 12

--- utils/functions.js
+++ utils/functions.js
@@ -1,4 +1,5 @@
 module.exports = {
-  manhattan: ([x1, y1], [x2, y2]) => Math.abs(x1 - x2) + Math.abs(y1 - y2),
+  manhattan: ([x1, y1], [x2, y2] = [0, 0]) =>
+    Math.abs(x1 - x2) + Math.abs(y1 - y2),
   strToIntArray: (str) => str.split('\n').map((number) => parseInt(number, 10)),
 };
