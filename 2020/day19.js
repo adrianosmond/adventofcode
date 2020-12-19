2a227c33d8260d6ad602d0843ad5a46eec3d4356
Adrian Osmond <a.c.osmond@gmail.com>
2020-12-19 16:10:31 +0100

    2020 day 19

---
+++ 2020/day19.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const path = require('path');
+
+const input = fs.readFileSync(path.resolve(__dirname, 'input19.txt'), 'utf8');
+
+const [r, m] = input.replace(/"/g, '').split('\n\n');
+
+const messages = m.split('\n');
+
+const solve = (rules) => {
+  let mergedRules = `${rules}\n`;
+  mergedRules = mergedRules.replace(/\n/g, ' \n');
+
+  while (mergedRules.indexOf('\n') >= 0) {
+    const [match, i, str] = mergedRules.match(/(\d+): ([^0-9\n$]+)\n/);
+    if (i === '0') break;
+    const hasPipe = str.indexOf('|') >= 0;
+    while (mergedRules.includes(` ${i} `)) {
+      mergedRules = mergedRules.replace(
+        new RegExp(` ${i} `, 'g'),
+        hasPipe ? ` (${str}) ` : ` ${str} `,
+      );
+    }
+    mergedRules = mergedRules.replace(match.trim(), '');
+    mergedRules = mergedRules.replace(/\n\n/g, '\n');
+  }
+  mergedRules = mergedRules.replace('0: ', '');
+  mergedRules = mergedRules.replace(/[\s]/g, '');
+
+  mergedRules = new RegExp(`^${mergedRules}$`);
+  return messages.reduce(
+    (total, message) => total + (mergedRules.test(message) ? 1 : 0),
+    0,
+  );
+};
+
+const part1 = () => solve(r);
+
+const part2 = () => {
+  let rules = r.replace('8: 42', '8: ( 42 )+');
+  rules = rules.replace(
+    '11: 42 31',
+    '11: 42 ( 42 ( 42  ( 42  31 )?  31 )? 31 )?  31',
+  );
+
+  return solve(rules);
+};
+
+console.log('part1', part1());
+console.log('part2', part2());
