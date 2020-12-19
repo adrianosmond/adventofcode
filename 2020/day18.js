28cb3b2d13048545680aacddd3d5d966066cd721
adrianosmond <adrian.osmond@ev-box.com>
2020-12-19 16:31:36 +0100

    2020 day 18

---
+++ 2020/day18.js
@@ -0,0 +1,57 @@
+const fs = require('fs');
+const path = require('path');
+const { sum } = require('../utils/reducers');
+
+const input = fs.readFileSync(path.resolve(__dirname, 'input18.txt'), 'utf8');
+
+const equations = input.split('\n');
+
+const evaluateLtoR = (equation) => {
+  let withoutBrackets = equation;
+  while (withoutBrackets.indexOf('(') >= 0) {
+    withoutBrackets = withoutBrackets.replace(/\([^())]+\)/g, (match) =>
+      evaluateLtoR(match.replace(/[()]/g, '')),
+    );
+  }
+
+  const e = withoutBrackets.split(' ');
+  let result = parseInt(e[0], 10);
+  for (let i = 1; i < e.length; i += 2) {
+    const operator = e[i];
+    const operand = parseInt(e[i + 1], 10);
+    if (operator === '*') {
+      result *= operand;
+    } else {
+      result += operand;
+    }
+  }
+
+  return result;
+};
+
+const evaluateAdditionFirst = (equation) => {
+  let withoutBrackets = equation;
+  while (withoutBrackets.indexOf('(') >= 0) {
+    withoutBrackets = withoutBrackets.replace(/\([^())]+\)/g, (match) =>
+      evaluateAdditionFirst(match.replace(/[()]/g, '')),
+    );
+  }
+
+  while (withoutBrackets.indexOf('+') >= 0) {
+    withoutBrackets = withoutBrackets.replace(/\d+ \+ \d+/g, (match) => {
+      const [, n1, n2] = match.match(/(\d+) \+ (\d+)/);
+      return parseInt(n1, 10) + parseInt(n2, 10);
+    });
+  }
+
+  return withoutBrackets
+    .split(' * ')
+    .reduce((total, current) => total * parseInt(current, 10), 1);
+};
+
+const part1 = () => equations.map(evaluateLtoR).reduce(sum);
+
+const part2 = () => equations.map(evaluateAdditionFirst).reduce(sum);
+
+console.log('part1', part1());
+console.log('part2', part2());
