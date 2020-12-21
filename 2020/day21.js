c513899cb38bae647ac8e0f595c82dbac95aa09c
Adrian Osmond <a.c.osmond@gmail.com>
2020-12-21 08:22:01 +0100

    2020 day 21

---
+++ 2020/day21.js
@@ -0,0 +1,62 @@
+/* eslint-disable no-loop-func */
+const fs = require('fs');
+const path = require('path');
+
+const input = fs.readFileSync(path.resolve(__dirname, 'input21.txt'), 'utf8');
+
+const data = input
+  .replace(/[()]/g, '')
+  .split('\n')
+  .map((r) => r.split(' contains ').map((i) => i.split(/[,]? /)));
+
+const possibilities = {};
+
+const part1 = () => {
+  const allIngredients = data.map(([i]) => i).flat();
+  data.forEach(([ingredients, allergens]) => {
+    allergens.forEach((allergen) => {
+      if (!possibilities[allergen]) {
+        possibilities[allergen] = ingredients;
+      } else {
+        possibilities[allergen] = [...possibilities[allergen]].filter((i) =>
+          ingredients.includes(i),
+        );
+      }
+    });
+  });
+
+  const possibleAllergens = Object.values(possibilities).flat();
+  const allergyFree = allIngredients.filter(
+    (i) => !possibleAllergens.includes(i),
+  );
+
+  return allergyFree.length;
+};
+
+const part2 = () => {
+  let changed = true;
+  const done = [];
+
+  while (changed) {
+    changed = false;
+    Object.entries(possibilities).forEach(([allergen, ingredients], index) => {
+      if (ingredients.length === 1 && !done.includes(allergen)) {
+        changed = true;
+        done.push(allergen);
+        Object.entries(possibilities).forEach(([a, i], idx) => {
+          if (idx === index) return;
+          possibilities[a] = i.filter((x) => x !== ingredients[0]);
+        });
+      }
+    });
+  }
+
+  return Object.entries(possibilities)
+    .map(([a, i]) => [a, i[0]])
+    .sort((a, b) => (a[0] < b[0] ? -1 : 1))
+    .map((a) => a[1])
+    .join(',');
+};
+
+console.log('part1', part1());
+console.log('part2', part2());
