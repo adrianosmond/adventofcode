ddc16f928c63dd04393e7b3afc355c72108de913
Adrian Osmond <a.c.osmond@gmail.com>
2021-12-02 13:16:27 +0100

    2021 day 2

---
+++ 2021/day02.js
@@ -0,0 +1,54 @@
+const fs = require('fs');
+const path = require('path');
+
+const input = fs.readFileSync(path.resolve(__dirname, 'input02.txt'), 'utf8');
+
+const instructions = input.split('\n').map((line) => {
+  const [, instruction, amount] = line.match(/([a-z]+) ([0-9]+)/);
+  return [instruction, parseInt(amount, 10)];
+});
+
+const part1 = () => {
+  let horizontal = 0;
+  let depth = 0;
+  instructions.forEach(([instruction, amount]) => {
+    switch (instruction) {
+      case 'forward':
+        horizontal += amount;
+        break;
+      case 'up':
+        depth -= amount;
+        break;
+      case 'down':
+        depth += amount;
+        break;
+      default:
+    }
+  });
+  return horizontal * depth;
+};
+
+const part2 = () => {
+  let horizontal = 0;
+  let depth = 0;
+  let aim = 0;
+  instructions.forEach(([instruction, amount]) => {
+    switch (instruction) {
+      case 'forward':
+        horizontal += amount;
+        depth += aim * amount;
+        break;
+      case 'up':
+        aim -= amount;
+        break;
+      case 'down':
+        aim += amount;
+        break;
+      default:
+    }
+  });
+  return horizontal * depth;
+};
+
+console.log('part1', part1());
+console.log('part2', part2());
