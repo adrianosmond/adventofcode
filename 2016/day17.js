985ccf377b97aa4dbfb9ffdb3c44512ed15f6cdf
Adrian Osmond <a.c.osmond@gmail.com>
2021-12-03 17:29:41 +0100

    2016 day 17

---
+++ 2016/day17.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const fs = require('fs');
+const md5 = require('./md5');
+
+const input = fs.readFileSync(path.resolve(__dirname, 'input17.txt'), 'utf8');
+
+const isOpen = (char) => /[b-f]/.test(char);
+
+const getNextNodes = ({ x, y, steps }) => {
+  const nextNodes = [];
+  const hash = md5.hashBinary(input + steps);
+  if (isOpen(hash[0]) && y > 0) {
+    nextNodes.push({ x, y: y - 1, steps: `${steps}U` });
+  }
+  if (isOpen(hash[1]) && y < 3) {
+    nextNodes.push({ x, y: y + 1, steps: `${steps}D` });
+  }
+  if (isOpen(hash[2]) && x > 0) {
+    nextNodes.push({ x: x - 1, y, steps: `${steps}L` });
+  }
+  if (isOpen(hash[3]) && x < 3) {
+    nextNodes.push({ x: x + 1, y, steps: `${steps}R` });
+  }
+  return nextNodes;
+};
+
+const findPath = () => {
+  const nodes = [{ x: 0, y: 0, steps: '' }];
+  while (nodes.length > 0) {
+    const node = nodes.shift();
+    if (node.x === 3 && node.y === 3) {
+      return node.steps;
+    }
+    nodes.push(...getNextNodes(node));
+  }
+};
+
+const findLongestPath = () => {
+  let longest = 0;
+  const nodes = [{ x: 0, y: 0, steps: '' }];
+  while (nodes.length > 0) {
+    const node = nodes.shift();
+    if (node.x === 3 && node.y === 3) {
+      if (node.steps.length > longest) longest = node.steps.length;
+      continue;
+    }
+    nodes.push(...getNextNodes(node));
+  }
+  return longest;
+};
+
+const part1 = () => findPath();
+
+const part2 = () => findLongestPath();
+
+console.log('part1', part1());
+console.log('part2', part2());
