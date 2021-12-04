dc6829eb7d0d7c47b9bc2633f32bdfe7c032dbb4
Adrian Osmond <a.c.osmond@gmail.com>
2021-12-04 06:58:11 +0100

    2016 day 18

---
+++ 2016/day18.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const fs = require('fs');
+
+const input = fs.readFileSync(path.resolve(__dirname, 'input18.txt'), 'utf8');
+const cols = input.length;
+
+const getChar = (left, center, right) => {
+  if (left === '^' && center === '^' && right === '.') {
+    return '^';
+  }
+  if (left === '.' && center === '^' && right === '^') {
+    return '^';
+  }
+  if (left === '^' && center === '.' && right === '.') {
+    return '^';
+  }
+  if (left === '.' && center === '.' && right === '^') {
+    return '^';
+  }
+  return '.';
+};
+
+const solve = (maxRows) => {
+  let safe = input.split('').filter((c) => c === '.').length;
+  let previous = input;
+  for (let i = 0; i < maxRows - 1; i++) {
+    let current = '';
+
+    for (let j = 0; j < cols; j++) {
+      let next;
+      if (j === 0) {
+        next = getChar('.', previous[0], previous[1]);
+      } else if (j === cols - 1) {
+        next = getChar(previous[cols - 2], previous[cols - 1], '.');
+      } else {
+        next = getChar(previous[j - 1], previous[j], previous[j + 1]);
+      }
+      if (next === '.') safe++;
+      current += next;
+    }
+    previous = current;
+  }
+
+  return safe;
+};
+
+const part1 = () => solve(40);
+
+const part2 = () => solve(400000);
+
+console.log('part1', part1());
+console.log('part2', part2());
