a1fdb28e88dc4c42b774d7c21201545e5bc4e254
Adrian Osmond <a.c.osmond@gmail.com>
2021-12-11 14:18:55 +0100

    2016 day 21

--- utils/functions.js
+++ utils/functions.js
@@ -3,4 +3,24 @@ module.exports = {
     Math.abs(x1 - x2) + Math.abs(y1 - y2),
   strToIntArray: (str, sep = '\n') =>
     str.split(sep).map((number) => parseInt(number, 10)),
+  permutator: (inputArr) => {
+    const results = [];
+
+    function permute(arr, memo = []) {
+      let cur;
+
+      for (let i = 0; i < arr.length; i++) {
+        cur = arr.splice(i, 1);
+        if (arr.length === 0) {
+          results.push(memo.concat(cur));
+        }
+        permute(arr.slice(), memo.concat(cur));
+        arr.splice(i, 0, cur[0]);
+      }
+
+      return results;
+    }
+
+    return permute(inputArr);
+  },
 };
