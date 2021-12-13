8afb8ea54eb521da7315110e212d7b12f7a5019f
Adrian Osmond <a.c.osmond@gmail.com>
2021-12-13 21:48:05 +0100

    day 12 rewrite for speed

--- 2021/day12.js
+++ 2021/day12.js
@@ -13,53 +13,31 @@ connections.forEach(([a, b]) => {
   else graph[b].connections.push(a);
 });
 
-const hasDuplicate = (p) => {
-  const small = p.filter((s) => s.toLowerCase() === s);
-  const unique = new Set([...small]);
-  return small.length !== unique.size;
+const getEmptyVisits = () =>
+  Object.fromEntries(Object.keys(graph).map((key) => [key, 0]));
+
+const hasDuplicate = (visits) => Object.values(visits).includes(2);
+
+const countPaths = (node, visits, allowDuplicates = false) => {
+  if (node === 'end') return 1;
+
+  let count = 0;
+  graph[node].connections.forEach((nextNode) => {
+    const isSmall = !graph[nextNode].big;
+    const cantVisitSmall =
+      visits[nextNode] > 0 && (allowDuplicates ? hasDuplicate(visits) : true);
+    if (nextNode === 'start' || (isSmall && cantVisitSmall)) return;
+
+    if (isSmall) visits[nextNode]++;
+    count += countPaths(nextNode, visits, allowDuplicates);
+    if (isSmall) visits[nextNode]--;
+  });
+  return count;
 };
 
-const countPaths = (allowDuplicates = false) => {
-  const paths = [['start']];
-  let changed = true;
-  while (changed) {
-    const newPaths = [];
-    changed = false;
-    for (let i = 0; i < paths.length; i++) {
-      const p = paths[i];
-      const last = p[p.length - 1];
-      if (last === 'end') {
-        continue;
-      }
-      changed = true;
-      paths.splice(i, 1);
-      i--;
-      const node = graph[last];
-      node.connections.forEach((c) => {
-        if (
-          c === 'start' ||
-          (!graph[c].big &&
-            c !== 'end' &&
-            p.includes(c) &&
-            (allowDuplicates ? hasDuplicate(p) : true))
-        )
-          return;
-        const clone = p.slice(0);
-        clone.push(c);
-        newPaths.push(clone);
-      });
-    }
-    newPaths.forEach((p) => {
-      paths.push(p);
-    });
-  }
+const part1 = () => countPaths('start', getEmptyVisits());
 
-  return paths.length;
-};
-
-const part1 = () => countPaths();
-
-const part2 = () => countPaths(true);
+const part2 = () => countPaths('start', getEmptyVisits(), true);
 
 console.log('part1', part1());
 console.log('part2', part2());
