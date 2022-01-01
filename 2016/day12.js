0c7bb5e45b3d6ff206fde71681f6d34a7051e35b
Adrian Osmond <a.c.osmond@gmail.com>
2022-01-01 14:18:39 +0100

    2016 day 25

--- 2016/day12.js
+++ 2016/day12.js
@@ -1,4 +1,5 @@
 const input = require('./input12');
+const runProgram = require('./assembunny');
 
 const instructions = input.split('\n').map((i) => {
   const [op, x, y] = i.split(' ');
@@ -9,40 +10,5 @@ const instructions = input.split('\n').map((i) => {
   return [op, xVal, yVal];
 });
 
-const regKey = { a: 0, b: 1, c: 2, d: 3 };
-let registers;
-let iPtr = 0;
-
-const opCodes = {
-  cpy: (X, Y) => {
-    registers[regKey[Y]] = typeof X === 'number' ? X : registers[regKey[X]];
-  },
-  inc: (X) => {
-    registers[regKey[X]]++;
-  },
-  dec: (X) => {
-    registers[regKey[X]]--;
-  },
-  jnz: (X, Y) => {
-    const xVal = typeof X === 'number' ? X : registers[regKey[X]];
-    const yVal = typeof Y === 'number' ? Y : registers[regKey[Y]];
-
-    if (xVal !== 0) {
-      iPtr += yVal - 1;
-    }
-  },
-};
-
-const runProgram = (initialRegisters) => {
-  registers = initialRegisters;
-  iPtr = 0;
-  while (iPtr < instructions.length) {
-    const [op, x, y] = instructions[iPtr];
-    opCodes[op](x, y);
-    iPtr++;
-  }
-  return registers[0];
-};
-
-console.log('part1:', runProgram([0, 0, 0, 0]));
-console.log('part2:', runProgram([0, 0, 1, 0]));
+console.log('part1:', runProgram([0, 0, 0, 0], instructions).next().value);
+console.log('part2:', runProgram([0, 0, 1, 0], instructions).next().value);
---
+++ 2016/day25.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const fs = require('fs');
+const runProgram = require('./assembunny');
+
+const input = fs.readFileSync(path.resolve(__dirname, 'input25.txt'), 'utf8');
+
+const instructions = input.split('\n').map((i) => {
+  const [op, x, y] = i.split(' ');
+  const xInt = parseInt(x, 10);
+  const yInt = parseInt(y, 10);
+  const xVal = Number.isNaN(xInt) ? x : xInt;
+  const yVal = Number.isNaN(yInt) ? y : yInt;
+  return [op, xVal, yVal];
+});
+
+const part1 = () => {
+  for (let i = 1; i < 1000; i++) {
+    let numOutputs = 0;
+    for (const output of runProgram([i, 0, 0, 0], instructions)) {
+      if (output !== numOutputs % 2) break;
+      numOutputs++;
+      if (numOutputs === 100) return i;
+    }
+  }
+};
+
+console.log('part1', part1());
