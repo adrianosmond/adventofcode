6b54641c6839c2fd50bb55c3313f5cff0ef0a806
Adrian Osmond <a.c.osmond@gmail.com>
2022-06-03 14:16:17 +0200

    2015 day 12

---
+++ 2015/day12.js
@@ -0,0 +1,20 @@
+import { readInput } from '../utils/functions.js';
+
+const input = readInput();
+const json = JSON.parse(input);
+
+const getSumOfValues = (vals, ignoreRed = false) =>
+  vals.reduce((total, val) => {
+    if (typeof val === 'string') return total;
+    if (typeof val === 'number') return total + val;
+    if (Array.isArray(val)) return total + getSumOfValues(val, ignoreRed);
+    if (ignoreRed && Object.values(val).includes('red')) return total;
+    return total + getSumOfValues(Object.values(val), ignoreRed);
+  }, 0);
+
+const part1 = () => getSumOfValues(Object.values(json));
+
+const part2 = () => getSumOfValues(Object.values(json), true);
+
+console.log('part1', part1());
+console.log('part2', part2());
