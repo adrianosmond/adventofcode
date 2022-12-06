1afaa50ca64d94933dec787fc621f92b79b835a4
Adrian Osmond <a.c.osmond@gmail.com>
2022-12-06 06:39:36 +0100

    2022 day 6

---
+++ 2022/day06.js
@@ -0,0 +1,18 @@
+import { readInput } from '../utils/functions.js';
+
+const input = readInput();
+
+const findSequenceOfLength = (length) => {
+  const chars = input.split('');
+  for (let i = length; i < chars.length; i++) {
+    if (new Set(chars.slice(i - length, i)).size === length) return i;
+  }
+  return -1;
+};
+
+const part1 = () => findSequenceOfLength(4);
+
+const part2 = () => findSequenceOfLength(14);
+
+console.log('part1', part1());
+console.log('part2', part2());
