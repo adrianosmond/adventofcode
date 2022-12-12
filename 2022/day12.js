9315f594137c5d3fc9da1ecdd3b72618af2744b5
Adrian Osmond <a.c.osmond@gmail.com>
2022-12-12 07:53:08 +0100

    2022 day 12

---
+++ 2022/day12.js
@@ -0,0 +1,63 @@
+import { getNeighbours, readInput } from '../utils/functions.js';
+
+const input = readInput();
+let start;
+let end;
+
+const map = input.split('\n').map((r, rIdx) =>
+  r.split('').map((c, cIdx) => {
+    const code = c.charCodeAt(0);
+    // replace S with a
+    if (code === 83) {
+      start = [rIdx, cIdx];
+      return 97;
+    }
+    // replace E with z
+    if (code === 69) {
+      end = [rIdx, cIdx];
+      return 122;
+    }
+    return code;
+  }),
+);
+
+const getShortestDistance = (grid, startPos) => {
+  const bestDistances = new Array(grid.length)
+    .fill(0)
+    .map(() => new Array(grid[0].length).fill(Number.MAX_SAFE_INTEGER));
+  bestDistances[startPos[0]][startPos[1]] = 0;
+
+  const queue = [startPos];
+  while (queue.length > 0) {
+    const [row, col] = queue.shift();
+    const currentHeight = map[row][col];
+    const currentDistance = bestDistances[row][col];
+    getNeighbours(grid, row, col).forEach(([r, c, height]) => {
+      if (
+        height - currentHeight <= 1 &&
+        currentDistance + 1 < bestDistances[r][c]
+      ) {
+        queue.push([r, c]);
+        bestDistances[r][c] = currentDistance + 1;
+      }
+    });
+  }
+  return bestDistances[end[0]][end[1]];
+};
+
+const part1 = () => getShortestDistance(map, start);
+
+const part2 = () => {
+  let best = Number.MAX_SAFE_INTEGER;
+  for (let r = 0; r < map.length; r++) {
+    for (let c = 0; c < map[0].length; c++) {
+      if (map[r][c] === 97) {
+        best = Math.min(best, getShortestDistance(map, [r, c]));
+      }
+    }
+  }
+  return best;
+};
+
+console.log('part1', part1());
+console.log('part2', part2());
