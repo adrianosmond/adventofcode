f773f30ef8222a3b69118cf01fe7ceb8b1a1842e
Adrian Osmond <adrian.osmond@werkspot.nl>
2023-12-04 20:32:10 +0100

    2023 day 4

---
+++ 2023/day04.js
@@ -0,0 +1,39 @@
+import { readInput } from '../utils/functions.js';
+import { sum } from '../utils/reducers.js';
+
+const input = readInput();
+const winningNumbersPerCard = input
+  .split('\n')
+  .map((c) =>
+    c
+      .replace(/\s+/g, ' ')
+      .replace(/Card \d+:\s/g, '')
+      .split(' | ')
+      .map((x) => x.split(' ').map((n) => parseInt(n, 10))),
+  )
+  .map(([winners, myNumbers]) => winners.filter((w) => myNumbers.includes(w)));
+
+const part1 = () =>
+  winningNumbersPerCard
+    .map((winningNumbers) =>
+      winningNumbers.length === 0 ? 0 : 2 ** (winningNumbers.length - 1),
+    )
+    .reduce(sum);
+
+const part2 = () => {
+  const counts = Array(winningNumbersPerCard.length).fill(1);
+  const scores = winningNumbersPerCard.map(
+    (winningNumbers) => winningNumbers.length,
+  );
+
+  for (let i = 0; i < scores.length; i++) {
+    for (let j = 1; j <= scores[i]; j++) {
+      counts[i + j] += counts[i];
+    }
+  }
+
+  return counts.reduce(sum);
+};
+
+console.log('part1', part1());
+console.log('part2', part2());
