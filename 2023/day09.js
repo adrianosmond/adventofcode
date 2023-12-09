9fb70ae56da01946986e8521f0245c99f9f79420
Adrian Osmond <a.c.osmond@gmail.com>
2023-12-09 06:31:05 +0100

    2023 day 9

---
+++ 2023/day09.js
@@ -0,0 +1,35 @@
+import { readInput } from '../utils/functions.js';
+import { sum } from '../utils/reducers.js';
+
+const input = readInput();
+
+const sequences = input
+  .split('\n')
+  .map((s) => s.split(' ').map((n) => parseInt(n, 10)));
+
+const getDiffs = (sequence) => {
+  const diffs = [];
+  for (let i = 1; i < sequence.length; i++) {
+    diffs.push(sequence[i] - sequence[i - 1]);
+  }
+  return diffs;
+};
+
+const getNextSequenceItem = (sequence) => {
+  if (sequence[0] === 0 && sequence[1] === 0) return 0;
+  const diffs = getDiffs(sequence);
+  return sequence.at(-1) + getNextSequenceItem(diffs);
+};
+
+const getPrevSequenceItem = (sequence) => {
+  if (sequence[0] === 0 && sequence[1] === 0) return 0;
+  const diffs = getDiffs(sequence);
+  return sequence[0] - getPrevSequenceItem(diffs);
+};
+
+const part1 = () => sequences.map(getNextSequenceItem).reduce(sum);
+
+const part2 = () => sequences.map(getPrevSequenceItem).reduce(sum);
+
+console.log('part1', part1());
+console.log('part2', part2());
