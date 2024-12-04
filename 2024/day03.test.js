7b84ac0ebc340ef523e515ad981381c9d92c6d58
Adrian Osmond <a.c.osmond@gmail.com>
2024-12-04 06:39:17 +0100

    2024 day 3 test

---
+++ 2024/day03.test.js
@@ -0,0 +1,13 @@
+import runTests from '../utils/tests.js';
+
+const testCases = [
+  [
+    `xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`,
+    [],
+    161,
+    [],
+    48,
+  ],
+];
+
+runTests(2024, 3, testCases);
