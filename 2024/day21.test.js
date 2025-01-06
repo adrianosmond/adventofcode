9f79e034e48e3951a3f801cb7efe8e28d4ed03a2
Adrian Osmond <a.c.osmond@gmail.com>
2025-01-06 19:25:42 +0100

    2024 day 21

---
+++ 2024/day21.test.js
@@ -0,0 +1,17 @@
+import runTests from '../utils/tests.js';
+
+const testCases = [
+  [
+    `029A
+980A
+179A
+456A
+379A`,
+    [],
+    126384,
+    [],
+    154115708116294,
+  ],
+];
+
+runTests(2024, 21, testCases);
